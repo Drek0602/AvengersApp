d5cadd07eaf222283b961147026da45c5f09997a
Tim Acosta <timacosta06@gmail.com>
2020-12-07 14:08:10 +0000

    added SplashScreen & Main storyboard

---
+++ AvengersApp/Controllers/SplashViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  AvengersApp
+//
+//  Created by Tim Acosta on 2/12/20.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    //MARK:- IBOutlets
+    @IBOutlet var activityIndicator: UIActivityIndicatorView?
+    
+    
+    
+    //MARK:- States
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator?.startAnimating()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+        
+    }
+    
+    //MARK:-Private functions
+    
+    private func navigateHome(){
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4)) { [weak self] in
+            
+            let storyboardAvengersHome = UIStoryboard(name: "AvengersHome", bundle: nil)
+            
+            if let destination = storyboardAvengersHome.instantiateInitialViewController(){
+                self?.navigationController?.setViewControllers([destination], animated: true)
+            }
+            
+            
+        }
+    }
+
+
+}
+
